6918aca2c12fcc5a1214e5a3fb997d1cc2d5feb4
Ville Kähärä <kahara.ville@gmail.com>
2019-06-27 10:03:02 +0300

    Lab01, SelfAssessment, Task1, Product class Serilization and Deserilization in C#

---
+++ WebAPI/Module1/Lab01SelfAssessment/ProductSeDe/Product.cs
@@ -0,0 +1,13 @@
+
+
+namespace ProductSeDe
+{
+
+    class Product
+    {
+        public int ID {get;set;}
+        public string Name {get;set;}
+        public double Price {get;set;}
+    }
+
+}
---
+++ WebAPI/Module1/Lab01SelfAssessment/ProductSeDe/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace ProductSeDe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Product myProduct = new Product() {ID = 0, Name = "Coffee", Price = 2.5};
+            //serialize product
+            string jSonString = SerializeProduct(myProduct);
+            Console.WriteLine("============== Product Seriliazed ==============");
+            Console.WriteLine(jSonString);
+            Console.WriteLine("==============");
+            Console.WriteLine();
+            Console.WriteLine("============== new assigned Product Deseriliazed ==============");
+            Product mySecondProduct = DeSerializeProduct(jSonString);
+            
+            //print product  object details details
+            Console.WriteLine();
+            Console.WriteLine("============== Objects' details before modification ==============");
+            DisplayProductDetails(myProduct);
+            Console.WriteLine("==============");
+            DisplayProductDetails(mySecondProduct);
+            Console.WriteLine("==============");
+
+            //modify second product to check if it has reference to the first beacuse the type of return from DeSerializeProduct function
+            Console.WriteLine();
+            Console.WriteLine("============== modifying second product details ==============");
+            mySecondProduct.ID =3;
+            mySecondProduct.Name = "Tea";
+            Console.WriteLine();
+            
+            //print serialized details after modification
+            Console.WriteLine(" ============== JSONs after modification ==============");
+            Console.WriteLine(SerializeProduct(myProduct));
+            Console.WriteLine("==============");
+            Console.WriteLine(SerializeProduct(mySecondProduct));
+            Console.WriteLine("==============");
+            Console.WriteLine();
+            //print product  object details details after modification
+            Console.WriteLine(" ============== Objects after modification ==============");
+            DisplayProductDetails(myProduct);
+            Console.WriteLine("==============");
+            DisplayProductDetails(mySecondProduct);
+            Console.WriteLine("==============");
+        }
+
+        public static string SerializeProduct(Product product) {
+            return JsonConvert.SerializeObject(product);
+        }
+
+        //returns new Object from serialized string
+        public static Product DeSerializeProduct(string productString) {
+                return JsonConvert.DeserializeObject<Product>(productString);
+        }
+
+        public static void DisplayProductDetails(Product product) {
+            Console.WriteLine($"Product ID: {product.ID}");
+            Console.WriteLine($"Product Name: {product.Name}");
+            Console.WriteLine($"Product Price: {product.Price}");
+        }
+
+    }
+}
