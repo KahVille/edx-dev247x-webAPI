67c7dd9afb6128170d3a18a6ab3956eb927ef512
Ville Kähärä <kahara.ville@gmail.com>
2019-07-24 08:07:40 +0300

    Module4, Lab02. Created RepositoryInitializationTest

--- WebAPI/Module4/Lab02DebugWebAPIWithUnitTest/WebServer.Test/FunctionalTest.cs
+++ WebAPI/Module4/Lab02DebugWebAPIWithUnitTest/WebServer.Test/FunctionalTest.cs
@@ -1,5 +1,6 @@
 using Xunit;
 using WebServer.Controllers;
+using WebServer.Models;
 
 namespace WebServer.Test
 {
@@ -12,6 +13,21 @@ namespace WebServer.Test
         {
             var controller = new ProductsController();
             Assert.NotNull(controller);
+        }
+
+        //Assert.NotNull() Failure of Repository.Products
+        [Fact]
+        public void RepositoryInitializationTest() {
+            Assert.NotNull(Repository.Products);
+            Assert.Equal(4,Repository.Products.Count);
+
+            foreach(var id in new int[] {0,1,2,3,}) {
+                Assert.True(Repository.Products.ContainsKey(id));
+            }
+
+            foreach(var key in Repository.Products.Keys) {
+                Assert.Equal(key,Repository.Products[key].ID);
+            }
         }
 
     }
