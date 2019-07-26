b8a40552549909d7ea0200e4c58c429cc20ac729
Ville Kähärä <kahara.ville@gmail.com>
2019-07-26 14:44:19 +0300

    Module4, FinalProject, Helper functions for tests

--- WebAPI/Module4/SelfAssessment/WorldWebServer.Test/EndToEndTest.cs
+++ WebAPI/Module4/SelfAssessment/WorldWebServer.Test/EndToEndTest.cs
@@ -1,7 +1,30 @@
+using System.Net.Http;
+using System.Net.Http.Headers;
+using WorldWebServer.Models;
+
 namespace WorldWebServer.Test
 {
-    public class EndToEndTest 
+    public class EndToEndTest
     {
+        private HttpClient GetHttpClient()
+        {
+            var httpClient = new HttpClient();
+            httpClient.BaseAddress = new System.Uri("http://localhost:5000");
+            var acceptType = new MediaTypeWithQualityHeaderValue("application/json");
+            httpClient.DefaultRequestHeaders.Clear();
+            httpClient.DefaultRequestHeaders.Accept.Add(acceptType);
+            return httpClient;
+        }
         
+        private bool SameCity(City c1, City c2) {
+            return c1.ID == c2.ID
+            && c1.Name == c2.Name
+            && c1.CountryCode == c2.CountryCode
+            && c1.District == c2.District
+            && c1.Population == c2.Population;
+        }
+
+        
+
     }
 }
